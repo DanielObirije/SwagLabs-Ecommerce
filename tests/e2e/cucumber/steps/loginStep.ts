a50cab2e0d5cbf8a0fdddcee6e74623c8d7494f3
mac <99664094+DanielObirije@users.noreply.github.com>
2026-04-20 10:44:44 +0100

    configuring my cucumber setup with allure

---
+++ tests/e2e/cucumber/steps/loginStep.ts
@@ -0,0 +1,11 @@
+import { PageManager } from "page/PageManager";
+import { Given, When, Then } from "@cucumber/cucumber";
+import { expect } from "@playwright/test";
+
+
+// Given("that I am on the login page",async function() {
+//     if (condition) {
+        
+//     }
+    
+// });
